b9de3f8b75d104d4f598ba113de937d88049af6a
irisa <iri18shu@gmail.com>
2020-07-16 10:12:42 +0900

    提出

--- fukuv0716/Form1.cs
+++ fukuv0716/Form1.cs
@@ -16,5 +16,16 @@ namespace fukuv0716
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
